387772be0e6193b7b0ad12e0479042d3baf6266d
Renanlb <renanlb98@gmail.com>
2023-08-22 12:37:46 -0300

    header's creation

---
+++ uolkut/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HeaderUserAccess from './pages/UserAccess/HeaderUserAccess'
+
+function App() {
+
+  return (
+    <>
+      <HeaderUserAccess/>
+    </>
+  )
+}
+
+export default App
